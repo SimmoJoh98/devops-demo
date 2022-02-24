d87766166d0ed616702bd30d7294772d218fd91b
Xeilon <simmojoh98@gmail.com>
2022-02-24 12:36:42 -0700

    rollbar error added

--- server/index.js
+++ server/index.js
@@ -6,6 +6,20 @@ app.get(`/`, (req,res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
+let students = []
+app.post('/api/student', (req,res) => {
+    let {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+    rollbar.log('student was added successfully', {author: 'johnny', type: 'manual', student: name})
+    res.status(200).send(students)
+})
+
+app.use(rollbar.errorHandler())
+
+
+
 // include and initialize the rollbar library with your access token
 const Rollbar = require('rollbar')
 const rollbar = new Rollbar({
