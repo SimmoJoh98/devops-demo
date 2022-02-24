359e8c39cc2ce352f8d219765eb4265fe67d0839
Xeilon <simmojoh98@gmail.com>
2022-02-24 12:10:59 -0700

    rollbar integration

--- server/index.js
+++ server/index.js
@@ -6,6 +6,23 @@ app.get(`/`, (req,res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'b60374cebe244bc8a5702cedee515c70',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
+
+
+
+
+
+
 const port = process.env.PORT || 4545
 
 app.listen(port, () => console.log(`listening on ${port}`))
